f2875dac1982418f3138c141913333fbe8c82b8c
Charlie Brooking <cbrookin@thoughtworks.com>
2019-04-11 00:27:25 +1000

    clean out to replace with axon 4 demo

--- src/main/kotlin/com/example/axondemo/query/LoanApplicationViewRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.axondemo.query
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface LoanApplicationViewRepository : JpaRepository<LoanApplicationView, String>
